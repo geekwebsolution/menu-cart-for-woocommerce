9b81ec9ed1251dccd91688cc1610cd7abaf4bba3
samarth geek <74664988+geek-samarth@users.noreply.github.com>
2024-02-13 11:43:17 +0530

    v1.9

--- front/menu-cart.php
+++ front/menu-cart.php
@@ -36,7 +36,7 @@ if (!class_exists('mcfw_menu_cart')) {
                         if ( $args->menu->term_id ==$value) {
                             $mcfw_menu_link = (isset($mcfw_flyout_options['flyout_status']) && $mcfw_flyout_options['flyout_contents'] == 'click') ? 'javascript:void(0);': $page_redirect_url;
                             $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page mcfw-menu '.$mcfw_currency.' '.(($mcfw_flyout_options['flyout_contents'] == 'hover') ?  'mcfw-menu-hover' : '').' '.(($mcfw_general_options['menu_cart_formats'] == 'price_items' || $mcfw_general_options['menu_cart_formats'] == 'items_price') ? 'mcfw-menu-no-icon' : '').'">
-                                    <a href="'.$mcfw_menu_link.'" class="mcfw-menu-list 123"> ';
+                                    <a href="'.$mcfw_menu_link.'" class="mcfw-menu-list"> ';
                                     if (($mcfw_general_options['menu_cart_formats'] != 'items_price') && ($mcfw_general_options['menu_cart_formats'] != 'price_items') ) {
                                         $items .= $output;
                                     }
